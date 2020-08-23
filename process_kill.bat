e489675307a6b3ff4baa0dff7af0d907513ffe5f
KentaGoto <gotokentag@gmail.com>
2020-08-23 11:01:53 +0900

    Update process_kill.bat

--- process_kill.bat
+++ process_kill.bat
@@ -3,26 +3,26 @@
 setlocal
 
 rem ****************************************************
-rem 指定のプロセスが指定の上限数に達していたら全部kill
+rem If the specified process reaches the maximum number of processes, all the processes are killed.
 rem ****************************************************
 
-rem プロセス名を指定
+rem Specify a process name
 set process_excel=EXCEL.EXE
 set process_firefox=firefox.exe
 
-rem プロセス数の上限値を指定
+rem Specify the maximum number of processes.
 set excel_process_upper_limit=1
 set firefox_process_upper_limit=1
 
-rem タスクリストからプロセス名をgrepしてプロセス数を取得
+rem Grep the process name from the task list and get the number of processes.
 for /F "usebackq" %%s in (`tasklist ^| grep -c ^"%process_excel%^"`) do set process_count_excel=%%s
 for /F "usebackq" %%s in (`tasklist ^| grep -c ^"%process_firefox%^"`) do set process_count_firefox=%%s
 echo EXCEL.EXE %process_count_excel%
 echo firefox.exe %process_count_firefox%
 
-rem プロセス数が上限数に達していたらkill
+rem If the maximum number of processes has been reached, kill
 if %excel_process_upper_limit% leq %process_count_excel% (
-    echo EXCEL.exeをkillする
+    echo EXCEL.exe繧談ill縺吶ｋ
     echo %date% %time% >> Log.txt
     taskkill /im %process_excel% /f >> Log.txt
     echo. >> Log.txt
@@ -30,7 +30,7 @@ if %excel_process_upper_limit% leq %process_count_excel% (
 	echo OK
 )
 if %firefox_process_upper_limit% leq %process_count_firefox% (
-	echo firefox.exeをkillする
+	echo firefox.exe繧談ill縺吶ｋ
 	echo %date% %time% >> Log.txt
     taskkill /im %process_firefox% /f >> Log.txt
     echo. >> Log.txt
